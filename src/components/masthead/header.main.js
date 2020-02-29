5d8595ea8d5474f44463508185364076aeb043e4
Joey Robinson <theamazingness@gmail.com>
2020-02-29 12:02:47 -0500

    Adjusting header/nav with MUI.

--- src/components/masthead/header.main.js
+++ src/components/masthead/header.main.js
@@ -1,37 +1,138 @@
-import React from "react"
-import Navigation from "./header.nav"
-import Image from "../image"
-import ExternalLink from "../links/link.external"
+import React, { useState } from "react"
+import AppBar from "@material-ui/core/AppBar"
+// import CssBaseline from "@material-ui/core/CssBaseline"
+import Divider from "@material-ui/core/Divider"
+import Drawer from "@material-ui/core/Drawer"
+import Hidden from "@material-ui/core/Hidden"
+import IconButton from "@material-ui/core/IconButton"
+import InboxIcon from "@material-ui/icons/MoveToInbox"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
+import MailIcon from "@material-ui/icons/Mail"
+import MenuIcon from "@material-ui/icons/Menu"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 
-const Header = () => (
-  <header className="header">
-    <h1>Quick Anime</h1>
-    <Navigation />
-    <span>
-      <ExternalLink
-        path="https://ko-fi.com/joeyrobinson"
-        message={
-          <img
-            height="36"
-            style={{ border: `0px`, height: `36px` }}
-            src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=2"
-            alt="Buy Me a Coffee at ko-fi.com"
-          />
-        }
-        aria-label="Buy Me A Coffee"
-      />
-      <ExternalLink
-        path="https://www.gatsbyjs.org/"
-        message={<Image alt="Powered By Gatsby" filename="gatsby-icon.png" />}
-        aria-label="Powered By Gatsby"
-      />
-      <ExternalLink
-        path="https://github.com/Joey-Robinson"
-        message={<Image alt="GitHub" filename="octocat.png" />}
-        aria-label="GitHub"
-      />
-    </span>
-  </header>
-)
+const drawerWidth = 217
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    background: "#121212",
+    color: "#fff",
+    width: drawerWidth,
+    borderRight: "1px solid grey",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}))
+
+const Header = props => {
+  const { container } = props
+  const classes = useStyles()
+  const theme = useTheme()
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  )
+
+  return (
+    <div className={classes.root}>
+      {/* <CssBaseline /> */}
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Quick Anime
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  )
+}
 
 export default Header
