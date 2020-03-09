77f21c8557bffa22dfaa1acaaf73725febae45ad
Joey Robinson <theamazingness@gmail.com>
2020-03-08 20:14:46 -0400

    Working on pagination for searched anime.

--- src/components/anime/anime.search.js
+++ src/components/anime/anime.search.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from "react"
 import TextField from "@material-ui/core/TextField"
 import { makeStyles } from "@material-ui/core/styles"
 import Flippy, { FrontSide, BackSide } from "react-flippy"
+import Image from "../image"
 
 const useStyles = makeStyles(theme => ({
   root: {
     borderRadius: ".5em",
+    input: {
+      color: "white",
+    },
     "& > *": {
       margin: theme.spacing(1),
       width: 200,
@@ -18,16 +22,23 @@ const AnimeSearch = () => {
   const [animeData, setAnimeData] = useState({ results: [] })
   const [searchedAnime, setSearchedAnime] = useState("")
   const [url, setUrl] = useState("")
+  const [currentPage, setCurrentPage] = useState(1)
 
   useEffect(() => {
     fetch(url)
       .then(response => response.json())
       .then(data => setAnimeData(data))
-  }, [url])
+  }, [currentPage, url])
 
   const changeHandler = event => {
     setSearchedAnime(event.target.value)
-    setUrl(`https://api.jikan.moe/v3/search/anime?q=${event.target.value}`)
+    setUrl(
+      `https://api.jikan.moe/v3/search/anime?q=${event.target.value}&page=${currentPage}`
+    )
+  }
+
+  const next = () => {
+    setCurrentPage(currentPage + 1)
   }
 
   return (
@@ -37,50 +48,90 @@ const AnimeSearch = () => {
           <TextField
             className={classes.margin}
             onChange={changeHandler}
+            InputLabelProps={{
+              style: {
+                color: "white",
+              },
+            }}
             id="anime-search"
             label="Search Anime"
             variant="filled"
             color="secondary"
+            InputProps={{
+              style: {
+                color: "white",
+              },
+            }}
           />
         </form>
+        <button onClick={next}>N</button>
       </div>
       <ul className="search--list">
-        {animeData.results.length === 0
-          ? "Search..."
-          : animeData.results.map(
-              ({ synopsis, mal_id, title, image_url, url }) => (
-                <li key={mal_id}>
-                  <Flippy
-                    flipOnHover={true}
-                    flipOnClick={false}
-                    flipDirection="horizontal"
-                    style={{ width: "225px", height: "335px" }}
-                  >
-                    <FrontSide>
-                      <div style={{ maxWidth: "225px", maxHeight: "335px" }}>
-                        <h5 style={{ textAlign: "center" }}>{title}</h5>
-                        <a href={url} title="_blank" rel="noopener noreferrer">
-                          <img src={image_url} alt={title} />
-                        </a>
-                      </div>
-                    </FrontSide>
-                    <BackSide
-                      style={{
-                        background: "#333333",
-                        color: "white",
-                      }}
+        {animeData.results.length === 0 ? (
+          <div
+            style={{
+              borderLeft: "none",
+              borderRight: "none",
+              borderTop: "none",
+              border: "none",
+            }}
+            className="search--list__empty"
+          >
+            <p
+              style={{
+                borderLeft: "none",
+                borderRight: "none",
+                borderTop: "none",
+              }}
+            >
+              Protect Nezuko by Searching for Anime
+            </p>
+            <Image style={{ border: "none", outline: "none" }} />
+          </div>
+        ) : (
+          animeData.results.map(
+            ({ synopsis, mal_id, title, image_url, url }) => (
+              <li key={mal_id}>
+                <Flippy
+                  flipOnHover={true}
+                  flipOnClick={false}
+                  flipDirection="horizontal"
+                  style={{ width: "225px", height: "335px" }}
+                >
+                  <FrontSide>
+                    <div
+                      className="search--list__back"
+                      style={{ maxWidth: "225px", maxHeight: "335px" }}
                     >
-                      <div>
-                        <h5 style={{ textAlign: "center", color: "white" }}>
-                          {title}
-                        </h5>
-                        <p style={{ padding: "10px 15px" }}>{synopsis}</p>
-                      </div>
-                    </BackSide>
-                  </Flippy>
-                </li>
-              )
-            )}
+                      <h5 style={{ textAlign: "center" }}>{title}</h5>
+                      <a href={url} title="_blank" rel="noopener noreferrer">
+                        <img src={image_url} alt={title} />
+                      </a>
+                    </div>
+                  </FrontSide>
+                  <BackSide
+                    style={{
+                      background: "#333333",
+                      color: "white",
+                    }}
+                  >
+                    <>
+                      <h5 style={{ textAlign: "center", color: "white" }}>
+                        {title}
+                      </h5>
+                      <p
+                        className="search--synopsis"
+                        style={{ padding: "10px 15px" }}
+                      >
+                        {synopsis}
+                      </p>
+                    </>
+                  </BackSide>
+                </Flippy>
+              </li>
+            )
+          )
+        )}
       </ul>
     </section>
   )
--- src/components/image.js
+++ src/components/image.js
@@ -2,39 +2,31 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-// Note: You can change "images" to whatever you'd like.
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `StaticQuery`: https://gatsby.dev/staticquery
+ */
 
-const Image = props => (
+const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        images: allFile {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                fluid(maxWidth: 600) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
+        placeholderImage: file(relativePath: { eq: "nezy.webp" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
       }
     `}
-    render={data => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(props.filename)
-      })
-      if (!image) {
-        return null
-      }
-
-      //const imageSizes = image.node.childImageSharp.sizes; sizes={imageSizes}
-      return <Img alt={props.alt} fluid={image.node.childImageSharp.fluid} />
-    }}
+    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
   />
 )
-
 export default Image
