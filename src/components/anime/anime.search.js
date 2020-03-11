9eeffb7ab185adec9df61fa7ae2edc087e37da5b
Joey Robinson <theamazingness@gmail.com>
2020-03-11 00:24:05 -0400

    Adjusted Buttons & Layout for search page. Created new styling and removed MUI buttons.

--- src/components/anime/anime.search.js
+++ src/components/anime/anime.search.js
@@ -2,9 +2,7 @@ import React, { useState } from "react"
 import TextField from "@material-ui/core/TextField"
 import { makeStyles } from "@material-ui/core/styles"
 import Flippy, { FrontSide, BackSide } from "react-flippy"
-import Image from "../image"
-import NextButton from "../genres/genre-buttons/next.button"
-import PreviousButton from "../genres/genre-buttons/previous.button"
+// import Image from "../image"
 
 // ! Check to see if a label is needed for <TextField />
 
@@ -50,9 +48,11 @@ const AnimeSearch = () => {
   const onSubmitHandler = event => {
     if (event.key === 13) {
       event.preventDefault()
+      setCurrentPage(1)
       searchCall()
     } else {
       event.preventDefault()
+      setCurrentPage(1)
       searchCall()
     }
   }
@@ -61,25 +61,14 @@ const AnimeSearch = () => {
     <section className="search">
       <div className="search--input">
         <form
-          className={classes.root}
+          className={`${classes.root} field`}
           onSubmit={onSubmitHandler}
           autoComplete="off"
           noValidate
         >
-          {animeData.results.length === 0 || currentPage === 1 ? (
-            ""
-          ) : (
-            <PreviousButton
-              onClick={previousPage}
-              className="search--input__previous"
-              message="Previous Page"
-            />
-          )}
-          <br />
           <TextField
-            autoFocus="true"
             value={searchedAnime}
-            className={classes.margin}
+            className={`${classes.margin} field--input`}
             onChange={onChangeHandler}
             InputLabelProps={{
               style: {
@@ -97,92 +86,81 @@ const AnimeSearch = () => {
               },
             }}
           />
-          <br />
-          <button className="search--input__submit" value="Search For Anime">
+          <button
+            className="search--input__submit field--submit"
+            value="Search For Anime"
+          >
             Search For Anime
           </button>
-          <br />
-          {animeData.results.length === 0 ? (
-            ""
+          {/* Fix This */}
+          {animeData.results.length === 0 || currentPage === 1 ? (
+            <span className="field--previous btns ripple">Disabled</span>
           ) : (
-            <NextButton onClick={nextPage} className="search--input__next" />
+            <button
+              onClick={previousPage}
+              className="field--previous btns ripple"
+            >
+              <span style={{ fontSize: "20px" }}>&larr;</span>
+              Previous Page
+            </button>
           )}
+          <button onClick={nextPage} className="field--next btns ripple">
+            Next Page &nbsp;&nbsp;{" "}
+            <span style={{ fontSize: "20px" }}>&rarr;</span>
+          </button>
         </form>
       </div>
       <ul className="search--list">
-        {animeData.results.length === 0 ? (
-          <div
-            style={{
-              borderLeft: "none",
-              borderRight: "none",
-              borderTop: "none",
-              border: "none",
-            }}
-            className="search--list__empty"
-          >
-            <p
-              style={{
-                borderLeft: "none",
-                borderRight: "none",
-                borderTop: "none",
-              }}
-            >
-              Protect Nezuko by Searching for Anime
-            </p>
-            <Image style={{ border: "none", outline: "none" }} />
-          </div>
-        ) : (
-          animeData.results.map(
-            ({ synopsis, mal_id, title, image_url, url }) => (
-              <li key={mal_id}>
-                <Flippy
-                  flipOnHover={true}
-                  flipOnClick={false}
-                  flipDirection="horizontal"
-                  style={{ width: "225px", height: "335px" }}
-                >
-                  <FrontSide>
-                    <div
-                      className="search--list__back"
-                      style={{ maxWidth: "225px", maxHeight: "335px" }}
-                    >
-                      <h5 style={{ textAlign: "center" }}>{title}</h5>
-                      <>
-                        <img src={image_url} alt={title} />
-                      </>
-                    </div>
-                  </FrontSide>
-                  <BackSide
-                    style={{
-                      background: "#333333",
-                      color: "white",
-                    }}
+        {animeData.results.map(
+          ({ synopsis, mal_id, title, image_url, url }) => (
+            <li key={mal_id}>
+              <Flippy
+                flipOnHover={true}
+                flipOnClick={false}
+                flipDirection="horizontal"
+                style={{ width: "225px", height: "335px" }}
+              >
+                <FrontSide>
+                  <div
+                    className="search--list__back"
+                    style={{ maxWidth: "225px", maxHeight: "335px" }}
                   >
+                    <h5 style={{ textAlign: "center" }}>{title}</h5>
                     <>
-                      <h5 style={{ textAlign: "center", color: "white" }}>
-                        {title}
-                      </h5>
-                      <p
-                        className="search--synopsis"
-                        style={{ padding: "10px 15px" }}
-                      >
-                        {synopsis}
-                      </p>
-                      <div style={{ textAlign: "center" }}>
-                        <a
-                          style={{ color: "#f50057" }}
-                          href={url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          View on MAL
-                        </a>
-                      </div>
+                      <img src={image_url} alt={title} />
                     </>
-                  </BackSide>
-                </Flippy>
-              </li>
-            )
+                  </div>
+                </FrontSide>
+                <BackSide
+                  style={{
+                    background: "#333333",
+                    color: "white",
+                  }}
+                >
+                  <>
+                    <h5 style={{ textAlign: "center", color: "white" }}>
+                      {title}
+                    </h5>
+                    <p
+                      className="search--synopsis"
+                      style={{ padding: "10px 15px" }}
+                    >
+                      {synopsis}
+                    </p>
+                    <div style={{ textAlign: "center" }}>
+                      <a
+                        style={{ color: "#f50057" }}
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        View on MAL
+                      </a>
+                    </div>
+                  </>
+                </BackSide>
+              </Flippy>
+            </li>
           )
         )}
       </ul>
